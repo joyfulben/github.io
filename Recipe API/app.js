f7a89a5173d3ec7a5632133ef27f4f2b264fda77
Ben Fisher <benfisher@Bens-MacBook-Pro.local>
2020-02-08 07:45:21 -0700

    ajax request with recipe is working

--- Recipe API/app.js
+++ Recipe API/app.js
@@ -1,3 +1,4 @@
+// TODO: Get ajax request working with recipes
 let idArr = []
 $(() => {
   $.ajax({
@@ -7,23 +8,22 @@ $(() => {
       console.log(data);
       // for (let i = 0; i < data.length; i++) {
       //   console.log(data[i].id)
-
+// TODO: once access, save recipe id in variable and use in following ajax request
 
   },() => {
       console.log('Bad request');
-    }
-  )
-  $.ajax({
-    url:'https://api.spoonacular.com/recipes/763716/information?apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
-  }).then (
-    (data) => {
-      console.log(data);
-    },() => {
-      console.log('Bad request');
-    }
-  )
-
+  
+  })
+  // )
+  // $.ajax({
+  //   url:'https://api.spoonacular.com/recipes/763716/information?apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
+  // }).then (
+  //   (data) => {
+  //     console.log(data);
+  //   },() => {
+  //     console.log('Bad request');
+  //   }
+  // )
+//TODO: get her done!
 
 })
-
-console.log($);
