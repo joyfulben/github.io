80ea2ef35882095a90ecbe3af44a520fa04bf1ec
Ben Fisher <benfisher@Bens-MacBook-Pro.local>
2020-02-08 07:50:29 -0700

    logging recipe ids

--- Recipe API/app.js
+++ Recipe API/app.js
@@ -5,14 +5,14 @@ $(() => {
     url:'https://api.spoonacular.com/recipes/search?query=asparagus&cuisine=french&number=5&apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
   }).then (
     (data) => {
-      console.log(data);
-      // for (let i = 0; i < data.length; i++) {
-      //   console.log(data[i].id)
+      for (let i = 0; i < data.results.length; i++) {
+        console.log(data.results[i].id)
+      }
 // TODO: once access, save recipe id in variable and use in following ajax request
 
   },() => {
       console.log('Bad request');
-  
+
   })
   // )
   // $.ajax({
