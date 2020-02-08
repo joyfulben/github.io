21a229f3d264ab4c506cde9454ed2a837d971cd6
Ben Fisher <benfisher@Bens-MacBook-Pro.local>
2020-02-07 20:57:25 -0700

    first commit of files for Recipe API

---
+++ Recipe API/app.js
@@ -0,0 +1,29 @@
+let idArr = []
+$(() => {
+  $.ajax({
+    url:'https://api.spoonacular.com/recipes/search?query=asparagus&cuisine=french&number=5&apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
+  }).then (
+    (data) => {
+      console.log(data);
+      // for (let i = 0; i < data.length; i++) {
+      //   console.log(data[i].id)
+
+
+  },() => {
+      console.log('Bad request');
+    }
+  )
+  // $.ajax({
+  //   url:'https://api.spoonacular.com/recipes/763716/information?apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
+  // }).then (
+  //   (data) => {
+  //     console.log(data);
+  //   },() => {
+  //     console.log('Bad request');
+  //   }
+  // )
+  //
+
+})
+
+console.log($);
