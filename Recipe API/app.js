4cf92553348345b76981d77cc6673f49508dc661
Ben Fisher <benfisher@Bens-MacBook-Pro.local>
2020-02-09 09:13:19 -0700

    looping through id array to access each recipe with that id

--- Recipe API/app.js
+++ Recipe API/app.js
@@ -1,30 +1,42 @@
 // TODO: Get ajax request working with recipes
 let idArr = []
-$(() => {
-  $.ajax({
-    url:'https://api.spoonacular.com/recipes/search?query=asparagus&cuisine=french&number=5&apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
+const query = 'chicken'
+const ajaxUseID = () => {
+  for (let j=0;j<idArr.length;j++) {
+$.ajax({
+    url:'https://api.spoonacular.com/recipes/'+idArr[j]+'/information?apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
   }).then (
     (data) => {
-      for (let i = 0; i < data.results.length; i++) {
-        idArr.push(data.results[i].id)
-        console.log(idArr);
-      }
+      console.log(data.title);
+    },() => {
+      console.log('Bad request');
+    }
+)
+  }
+}
+// ajax request for basic recipe info using a user generated query
+const ajaxIDRecipe = () => {
+
+$.ajax({
+  url:'https://api.spoonacular.com/recipes/search?query='+ query + '&number=4&apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
+}).then (
+  (data) => {
+    for (let i = 0; i < data.results.length; i++) {
+      idArr.push(data.results[i].id);
+    }
+      ajaxUseID();
+
 // TODO: once access, save recipe id in variable and use in following ajax request
 
-  },() => {
-      console.log('Bad request');
+},() => {
+    console.log('Bad request');
 
-  })
-  // )
-  // $.ajax({
-  //   url:'https://api.spoonacular.com/recipes/763716/information?apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
-  // }).then (
-  //   (data) => {
-  //     console.log(data);
-  //   },() => {
-  //     console.log('Bad request');
-  //   }
-  // )
-//TODO: get her done!
+})
+}
+
+
+$(() => {
+
+  ajaxIDRecipe()
 
 })
