5413f2680e5d4e9f53b2f6127b90a943c64c0687
Ben Fisher <benfisher@Bens-MacBook-Pro.local>
2020-02-10 08:49:01 -0700

    filter buttons disappear after submitting query

--- Recipe API/app.js
+++ Recipe API/app.js
@@ -3,6 +3,7 @@
 //===========Array variables========//
 //==================================//
 let query = []
+let number = [2]
 let cuisine = []
 let diet = []
 let exceptions = []
@@ -12,18 +13,10 @@ let idArr = []
 //==========Functions===============//
 //==================================//
 // TODO: Create a function that when filters button is pushed, four buttons appear in its place, each with one of the four filters
-const inputFilterInputs = () => {
+// TODO: Take filter arrays and input them into ajaxIDRecipe
 
 
-
-}
-const inputFilter = () => {
-  event.preventDefault();
-  console.log($filter);
-  pushInputToFilterArr();
-  console.log(cuisine);
-  $($inputBox).val('')
-}
+//When a filter is chosen, a submit box with button is appended to original target filter button. The new submit button, recently appended to the target filter will push input to its respective array.
 const chooseFilter = () => {
   const $target = event.target;
   const $form = $('<form>')
@@ -45,64 +38,66 @@ const chooseFilter = () => {
       } else {
         intolerances.push($($inputBox).val())
       }
-
-      console.log(cuisine);
+      $($inputBox).val('')
   })
 }
 
-const submitFilter = () => {
-  if ($filter == 'Cuisine') {
-    cuisineArr.push
-  }
-}
 const filtersClick = () => {
   $('#options').on('click', () => {
     $('.filters').toggleClass('filters')
     $('.see-me').one('click', () => {
       chooseFilter(event)
-
     })
   })
 }
 
 // ajax request to use the idea acquired from previous ajax request
 const ajaxUseID = () => {
-
   for (let j=0;j<idArr.length;j++) {
     $.ajax({
       url:'https://api.spoonacular.com/recipes/'+idArr[j]+'/information?apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
     }).then (
       (data) => {
         console.log(data.title);
+
       },() => {
         console.log('Bad request');
       }
     )
   }
+  $('.see-me').remove()
 }
 // TODO: Set multiple variables that will take filters: cuisine, diet, excludeIngredients and intolerances to be added to ajaxIDRecipe in addition to query.
+
+//===========Submitting number of queries displayed============
+// const numberSubmit = () => {
+//   $('#number').on('submit', (event) => {
+//     event.preventDefault();
+//     console.log($('input[type=\'text\']').val());
+//   //   $('input[type=\'text\']').val('')
+//   })
+// }
+//=====================================================
+
 // ajax request for basic recipe info using a user generated query w/ or w/o filters
 const ajaxIDRecipe = () => {
-
-  $('form').on('submit', (event) => {
+  //=========Submitting recipe queries===============
+  $('#recipe').on('submit', (event) => {
     //Allows the value of input box to be used
     event.preventDefault();
     //Pushing the value of the input box to query variable.
     query.push($('input[type=\'text\']').val())
     //Reset input box by having its value be an empty string
   $('input[type=\'text\']').val('');
-
-
+  //====================================================
     $.ajax({
-      url:'https://api.spoonacular.com/recipes/search?query='+ query + cuisine + '&number=4&apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
+      url:'https://api.spoonacular.com/recipes/search?query='+query[0]+ '&cuisine='+cuisine+'&diet='+diet+'&excludeIngredients='+exceptions+'&intolerances='+intolerances+ '&number='+number+'&apiKey=a69a37e97ec54198ad3e9025f3f83e3d'
     }).then (
       (data) => {
         for (let i = 0; i < data.results.length; i++) {
           idArr.push(data.results[i].id);
         }
       ajaxUseID();
-
-
       },() => {
         console.log('Bad request');
       })
@@ -113,6 +108,7 @@ const ajaxIDRecipe = () => {
 
 //jquery on-load ready function
 $(() => {
+
   filtersClick()
   ajaxIDRecipe()
 
